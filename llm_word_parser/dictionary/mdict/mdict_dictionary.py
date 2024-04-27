e5c0375f1c8cae77c262991f145fad4d4e452671
Joey <mycityofsky@gmail.com>
2024-04-27 22:43:03 +0800

    add query to MDict

--- llm_word_parser/dictionary/mdict/mdict_dictionary.py
+++ llm_word_parser/dictionary/mdict/mdict_dictionary.py
@@ -1,15 +1,18 @@
 from typing import Optional
 
 from llm_word_parser.dictionary import Dictionary, DictionaryType
+from llm_word_parser.dictionary.mdict.query.mdict_query import IndexBuilder
 
 
 class MDict(Dictionary):
-    def __init__(self, id: Optional[int], name: str, path: str, active: bool = True):
+    def __init__(self, id: Optional[int], name: str, mdx_file_path: str, active: bool = True):
         self.id = id
         self.name = name
-        self.path = path
+        self.path = mdx_file_path
         self.type = DictionaryType.mdict
         self.active = active
+        self.mdx_builder = IndexBuilder(mdx_file_path)
 
-    def query(self, word: str) -> str:
-        pass
+    def query(self, word: str) -> Optional[str]:
+        definitions = self.mdx_builder.mdx_lookup(word)
+        return definitions[0] if definitions else None
